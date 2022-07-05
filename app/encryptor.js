8d67082891f9c5f346598803f5262f90355b0f85
Victor <vitek.borodyn@gmail.com>
2022-07-05 23:28:45 +0300

    Refactor Encryptor's class

--- app/encryptor.js
+++ app/encryptor.js
@@ -1,11 +1,11 @@
-import secureRandom from "crypto-random-string";
 import crypto from "crypto";
+import secureRandom from "crypto-random-string";
 
 export default class Encryptor {
   generateKey() {
     return secureRandom({ length: 32 });
   }
-  generateHMAC(message, key) {
+  generateHMAC(key, message) {
     return crypto.createHmac("sha256", key).update(message).digest("hex");
   }
 }
